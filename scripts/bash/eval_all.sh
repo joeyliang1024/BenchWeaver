3a229a4d9d9853dd8ba37a15de33ee71b930bdba
joeyliang1024 <joeyqazwsxedc@gmail.com>
2025-04-29 15:39:58 +0800

    Update some scores, fix datasets

--- scripts/bash/eval_all.sh
+++ scripts/bash/eval_all.sh
@@ -2,7 +2,7 @@
 export JAVA_HOME="/usr/lib/java"
 
 # Set the directory (can be modified or passed as an argument)
-TASK_NAME="ccpm"
+TASK_NAME="chinese-safety-qa"
 DIRECTORY="/work/u5110390/BenchWeaver/config/main_pipeline/$TASK_NAME"
 LOG_DIR="/work/u5110390/BenchWeaver/logs/main_pipeline/$TASK_NAME"
 
